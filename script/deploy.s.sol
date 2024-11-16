dd66ccb7a6219a4c11fe8e9ff94448b54dc94d69
divine-comedian <divine_comedian@protonmail.com>
2024-11-16 13:16:05 +0700

    updafe toml and script

---
+++ script/deploy.s.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import {UnionRings} from "../src/UnionRings.sol";
+import {CivilRegistry} from "../src/CivilRegistry.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+
+
+contract testDeploy is Script {
+    TransparentUpgradeableProxy ringSelectorProxy;
+    UnionRings ringSelector;
+    CivilRegistry civilRegistry;
+    ProxyAdmin proxyAdmin;
+    string private ringData = "";
+    string private description = "An ever growing collection of rings that are used to attest to Unions";
+    string private name = "Union Rings";
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PK");
+        address deployerPublicKey = vm.envAddress("PUBLIC_KEY");
+        uint64 schema = 0x4c;
+        vm.startBroadcast(deployerPrivateKey);
+
+        // proxyAdmin = new ProxyAdmin();
+        // ringSelectorImplementation = new UnionRings();
+        // ringSelectorProxy = new TransparentUpgradeableProxy(
+        //     address(ringSelectorImplementation),
+        //     address(proxyAdmin),
+        //     abi.encodeWithSelector(UnionRings(ringSelector).initialize.selector)
+        // );
+        // ringSelector = UnionRings(address(ringSelectorProxy));
+
+        // deploy union rings contract
+     address unionRingsProxy = Upgrades.deployTransparentProxy(
+    "UnionRings.sol",
+    msg.sender,
+    abi.encodeCall(UnionRings.initialize, (deployerPublicKey, deployerPublicKey))
+);
+
+
+    // deploy civil registry contract
+    address civilRegistryProxy = Upgrades.deployTransparentProxy(
+    "CivilRegistry.sol",
+    msg.sender,
+    abi.encodeCall(CivilRegistry.initialize, (unionRingsProxy, schema))
+);
+    console.log("UnionRings deployed at: ", unionRingsProxy);
+    console.log("CivilRegistry deployed at: ", civilRegistryProxy);
+}
+    }
--- src/UnionRings.sol
+++ src/UnionRings.sol
@@ -7,7 +7,7 @@ import "@openzeppelin-contracts-upgradeable/access/AccessControlUpgradeable.sol"
 import "@openzeppelin-contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol";
 import "@openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract MyToken is Initializable, ERC1155Upgradeable, AccessControlUpgradeable, ERC1155BurnableUpgradeable {
+contract UnionRings is Initializable, ERC1155Upgradeable, AccessControlUpgradeable, ERC1155BurnableUpgradeable {
     bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
